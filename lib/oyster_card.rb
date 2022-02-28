375b81d01b13b923f48579c96d989440667a178f
Felix Taljaard <felixtaljaard@hotmail.com>
2022-02-28 16:33:45 +0000

    Step 8 and 9

--- lib/oyster_card.rb
+++ lib/oyster_card.rb
@@ -1,14 +1,27 @@
 class Oystercard 
   def initialize
     @balance = 0
+    @in_journey = false
   end 
 
-  attr_reader :balance
+  attr_reader :balance, :in_journey
   LIMIT = 90
 
   def top_up(value)
     raise "Your balance is now £#{@balance}. Maximum limit of £90 reached" if limit_reached?
     @balance += value
+  end
+
+  def deduct(value)
+    @balance -= value
+  end
+
+  def touch_in
+    @in_journey = true
+  end
+
+  def touch_out
+    @in_journey = false
   end
 
   private
--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -6,6 +6,9 @@ describe Oystercard do
     it "respond to balance" do
       expect(subject).to respond_to(:balance)
     end 
+  end
+
+  describe "top_up" do
     it 'should top up balance' do
       expect(subject).to respond_to(:top_up).with(1).argument
       expect{subject.top_up(5)}.to change {subject.balance}.by(5)
@@ -14,5 +17,25 @@ describe Oystercard do
       subject.top_up(90)
       expect{ subject.top_up(1) }.to raise_error "Your balance is now £90. Maximum limit of £90 reached" if subject.balance >= Oystercard::LIMIT
     end 
+  end
+
+  describe "deduct" do
+    it 'should reduce balance by an amount' do
+    expect(subject).to respond_to(:deduct).with(1).argument
+    subject.top_up(5)
+    expect{subject.deduct(5)}.to change {subject.balance}.by(-5)
+    end
+  end
+
+  describe "in_journey" do
+    it 'should return true if touched in' do
+      subject.touch_in
+      expect(subject.in_journey).to eq true
+    end
+    it 'should return false if touched out' do
+      subject.touch_in
+      subject.touch_out
+      expect(subject.in_journey).to eq false
+    end
   end
 end
